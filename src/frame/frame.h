7f395b16b573be4e56421c28cb7d4ffc3238ffb6
ensar <ensar.makas@gmail.com>
2023-03-14 15:11:21 +0300

    added frame struct

---
+++ src/frame/frame.h
@@ -0,0 +1,14 @@
+#ifndef FRAME_H
+# define FRAME_H
+
+struct s_frame_1920x1080 {
+    int frame[1080][1920];
+};
+
+typedef struct s_frame_1920x1080 t_frame_1920x1080;
+
+t_frame_1920x1080 get_frame_1920x1080();
+
+void frame_set_pixel(unsigned int row, unsigned int col, int val);
+
+#endif
