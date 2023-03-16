1752747e2101e619b90d5e5795e17a120f3e3cc4
ensar <ensar.makas@gmail.com>
2023-03-16 21:40:48 +0300

    update@src/main: Frame printing moved into another function

--- src/main.c
+++ src/main.c
@@ -1,7 +1,20 @@
 #include <unistd.h>
-#include <mlx.h>
 #include <frame/frame.h>
 #include <stdio.h>
+#include <mlx.h>
+
+void print_frame(){
+
+    t_frame_1920x1080 frame;
+
+    frame = get_frame_1920x1080();
+
+    for(int i = 0; i < 1080; i++){
+        for (int j = 0; j < 1920; j++){
+            mlx_pixel_put(frame.mlx, frame.win, i,j,frame.canvas[i][j]);
+        }
+    }
+}
 
 int main(int ac, char **av) {
 
@@ -9,19 +22,19 @@ int main(int ac, char **av) {
     (void) av;
 
 	printf("burada miii?\n");
+    
     t_frame_1920x1080 frame;
 
     frame = get_frame_1920x1080();
-
+    
     for(int i = 0; i < 1080; i++){
         for (int j = 0; j < 1920; j++){
-            printf("setting pixel at [%d, %d]", i, j);
             frame_set_pixel(i, j, 255);
         }
     }
     printf("DERT TASA\n");
-
-    mlx_put_image_to_window(frame.mlx, frame.win, frame.canvas, 0, 0);
+    (void) frame;
+    print_frame();
     mlx_loop(frame.mlx);
     return (0);
 }
