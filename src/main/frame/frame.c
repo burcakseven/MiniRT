a35404596d3b9d3fe3f92a1d15e1eedcd0a3373d
burcakseven <burcakseven123@gmail.com>
2023-03-31 18:48:50 +0300

    printing colorful screen

--- src/main/frame/frame.c
+++ src/main/frame/frame.c
@@ -1,25 +1,48 @@
-/*#include "frame.h"
-#include <unistd.h>
-#include <mlx.h>
-#include <stdio.h>
-static t_frame_1920x1080	*frame_1920x1080(void)
-{
-	static t_frame_1920x1080	frame;
+#include "frame.h"
 
-	return &frame;
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
+}
+void print_color(t_data *img)
+{
+
+    for (int j = HEIGHT-1; j >= 0; --j) {
+        for (int i = 0; i < WIDTH; ++i) {
+            double r = (double)i / (WIDTH-1);
+            double g = (double)j / (HEIGHT-1);
+            double b = 0.25;
+
+            int ir = (int)(255.999 * r);
+            int ig = (int)(255.999 * g);
+            int ib = (int)(255.999 * b);
+			int rgb = (ir << 16) | (ig << 8) | ib;
+			my_mlx_pixel_put(img, i, j, rgb);
+        }
+    }
+
 }
 
-
-
-t_frame_1920x1080 get_frame_1920x1080()
+void	init_mlx(void)
 {
-	return (*(frame_1920x1080()));
+	void	*mlx;
+	void	*mlx_win;
+	t_data	img;
+
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, WIDTH, HEIGHT, "Hello world!");
+	img.img = mlx_new_image(mlx, WIDTH, HEIGHT);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
+								&img.endian);
+	print_color(&img);
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+	mlx_loop(mlx);
 }
 
-void frame_set_pixel(unsigned int row, unsigned int col, int val)
+int	main()
 {
-	t_frame_1920x1080 *frame = frame_1920x1080();
-
-	frame->canvas[row][col] = val;
+	init_mlx();
 }
-*/
