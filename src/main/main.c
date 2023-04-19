3e32c6243e324b0308b493d8126b77d31973a956
ensar <ensar.makas@gmail.com>
2023-04-19 13:53:11 +0300

    removed comment lines in main.c

--- src/main/main.c
+++ src/main/main.c
@@ -77,33 +77,6 @@ void reduce_color(t_light light, point3 intercept, color *colour){
     return ;
 }
 
-//void render_scene(t_scene scene){
-//    color       colour;
-//    point3      intersection;
-//    t_ray       ray_h;
-//    t_ray       ray_v;
-//    t_ray       ray;
-
-//    ray_v = make_ray((point3){0,-scene.camera.viewport_height,1},(point3){0,scene.camera.viewport_height,1});
-//    ray_h = make_ray((point3){-scene.camera.viewport_width,0,1},(point3){scene.camera.viewport_width,0,1});
-//    ray.orig = convert_point3(scene.camera.coordinate);
-//    ray.dir.y = -scene.camera.viewport_height;
-//    ray.dir.z = 1;
-//    int v_index = 0;
-//    while (ray.dir.y < scene.camera.viewport_height) {
-//        ray.dir.x = -scene.camera.viewport_width;
-//        int h_index = 0;
-//        while (ray.dir.x < scene.camera.viewport_width){
-
-//            colour = ray_color(&ray);
-//            put_pixel_to_img(h_index++,v_index,colour);
-//            ray.dir.x += (double) (2 * scene.camera.viewport_height) / HEIGHT;
-//        }
-//        v_index++;
-//        ray.dir.y += (double) (2 * scene.camera.viewport_height) / HEIGHT;
-//    }    
-//}
-
 void render_scene(t_scene scene) {
     color colour;
     //point3 intersection;
@@ -125,43 +98,6 @@ void render_scene(t_scene scene) {
         h++;
     }
 }
-
-//void render_scene(t_scene scene){
-//    color       colour;
-//    point3      intersection;
-//    t_ray       ray;
-
-
-//    ray.orig = convert_point3(scene.camera.coordinate);
-
-//    //printf("old vec: (%Lf,%Lf,%Lf) -> (%Lf, %Lf, %Lf)\n",ray.orig.x,ray.orig.y,ray.orig.z,ray.dir.x,ray.dir.y,ray.dir.z);
-    
-//    ray.dir = vec3_add(convert_point3(scene.camera.v_orientation), convert_point3(scene.camera.coordinate));
-    
-//    ray.dir = rotate_xz(ray.dir,-(double)scene.camera.fov/2.0);
-    
-//    ray.dir = rotate_yz(ray.dir,-(double)scene.camera.fov/2.0);
-
-//    printf("rotated vec: (%Lf,%Lf,%Lf) -> (%Lf, %Lf, %Lf)\n",ray.orig.x,ray.orig.y,ray.orig.z,ray.dir.x,ray.dir.y,ray.dir.z);
-
-
-//    int v_index = 0;
-//    // render image
-//    while (v_index < HEIGHT){
-        
-//        int h_index = 0;
-//        while (h_index < WIDTH) {
-//            colour = ray_color(&ray);
-//            put_pixel_to_img(h_index,v_index,colour);
-//            printf("[%Lf, %Lf, %Lf]\n",ray.dir.x,ray.dir.y,ray.dir.z);
-//            ray.dir = rotate_xz(ray.dir,((double) scene.camera.fov / WIDTH));
-//            h_index++;
-//        }
-//        ray.dir = rotate_yz(ray.dir,((double) scene.camera.fov / HEIGHT));
-//        v_index++;
-//    }
-    
-//}
 
 void show_img(){
     t_mlx mlx;
