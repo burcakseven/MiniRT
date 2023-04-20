519f4214002baf65be6bbeefcecb476ea832a364
burcakseven <burcakseven123@gmail.com>
2023-04-20 20:51:03 +0300

    root moot fonksiyonu koydum

--- src/main/frame/hit_objects.c
+++ src/main/frame/hit_objects.c
@@ -5,7 +5,26 @@ double discriminant(double a,double b, double c)
 	return (b * b - 4 * a * c);
 }
 
-int hit_sphere(const point3 center, double rad, const t_ray r)
+t_root roots(double a,double b, double discriminant)
+{
+	t_root root;
+	if(discriminant < 0)
+		root.root_number = 0;
+	else if (discriminant == 0 )
+	{
+		root.root_number = 1;
+		root.root1 = -b / (-2 * a);
+	}
+	else
+	{
+		root.root_number = 2;
+		root.root1 = (-b + sqrt(discriminant)) / (-2 * a);
+		root.root2 = (-b - sqrt(discriminant)) / (-2 * a);
+	}
+	return root;
+}
+
+t_root hit_sphere(const point3 center, double rad, const t_ray r)
 {
     t_vec3 oc;
 
@@ -13,7 +32,7 @@ int hit_sphere(const point3 center, double rad, const t_ray r)
     double a = vec3_dot(&r.dir, &r.dir);
     double b = 2.0 * vec3_dot(&oc, &r.dir);
     double c = vec3_dot(&oc,&oc) - rad*rad;
-    return (discriminant(a,b,c));
+    return (roots(a,b,discriminant(a,b,c)));
 }
 
 
@@ -27,19 +46,20 @@ double hit_plane(const point3 center, const point3 normal_vec, const t_ray r)
     return t;
 }
 
-
-double hit_cylinder(const point3 center, const point3 normal_vec,\
- const t_ray r, const double radius)
+t_root hit_cylinder(point3 center, const point3 normal_vec,\
+ const t_ray r, const double radius, const double height) // sınırsız silindir?
 {
 	double a;
 	double b;
 	double c;
     t_ray new_r;
 
+	center = transformed_ray(center,r,1).orig;
 	new_r = transformed_ray(normal_vec,r,1);
+	if(center.z - new_r.dir.z )
 	a = pow(new_r.dir.x,2) + pow(new_r.dir.y,2);
 	b = 2 *(new_r.orig.x * new_r.dir.x + new_r.orig.y * new_r.dir.y);
 	c = pow(new_r.orig.x,2) + pow(new_r.orig.y,2) - pow(radius,2);
 
-	return(discriminant(a,b,c));
+	return(roots(a,b,discriminant(a,b,c)));
 }
