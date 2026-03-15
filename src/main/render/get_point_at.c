2cc053e29990e940876f7ac0662b569506e2a6f4
burcakseven <burcakseven123@gmail.com>
2026-03-16 00:22:20 +0300

    changed get_point_at due to top_left starting

--- src/main/render/get_point_at.c
+++ src/main/render/get_point_at.c
@@ -6,15 +6,11 @@ static t_vec3 vec3_add3(t_vec3 v1, t_vec3 v2, t_vec3 v3) {
 
 #include <stdio.h>
 point3 get_point_at(t_virtural_canvas c, int x, int y) {
-  point3 pnt_tl;
-  point3 pnt_px;
 
   point3 scaledx = vec3_scale(c.v_unit_h, x);
   point3 scaledy = vec3_scale(c.v_unit_v, y);
-  pnt_tl = vec3_add3(c.norm_i, c.norm_j, c.norm_k);
-  pnt_px = vec3_add(scaledx, scaledy);
+  point3 ray_dir = vec3_add(c.canva_h.orig,vec3_add(scaledx, scaledy));
   // printf("scaledX[%Lf,%Lf,%Lf]\n",scaledx.x,scaledx.y,scaledx.z);
-  point3 res = vec3_add3(pnt_tl, pnt_px, c.camera_pos);
 
-  return (res);
+  return (vec3_normalize(ray_dir));
 }
